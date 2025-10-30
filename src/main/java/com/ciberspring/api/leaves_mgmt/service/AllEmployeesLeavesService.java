dae771b5e4049e08e720c6451b3421ab9e2fbe75
Sripavithra Manogaran <smanogaran@ciberspring.com>
2025-10-30 19:23:41 +0530

    Personal details removed

--- src/main/java/com/ciberspring/api/leaves_mgmt/service/AllEmployeesLeavesService.java
+++ src/main/java/com/ciberspring/api/leaves_mgmt/service/AllEmployeesLeavesService.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import com.ciberspring.api.leaves_mgmt.entity.EmployeeLeaveBalances;
 
+
 public interface AllEmployeesLeavesService {
-	List<EmployeeLeaveBalances> getAllEmployeesLeavesBalance();
+	 List<EmployeeLeaveBalances> getAllEmployeesLeavesBalance();
 
 }
--- src/main/java/com/ciberspring/api/leaves_mgmt/service/impl/AllEmployeesLeavesServiceImpl.java
+++ src/main/java/com/ciberspring/api/leaves_mgmt/service/impl/AllEmployeesLeavesServiceImpl.java
@@ -1,5 +1,4 @@
 package com.ciberspring.api.leaves_mgmt.service.impl;
-
 import java.util.List;
 import org.springframework.stereotype.Service;
 import com.ciberspring.api.leaves_mgmt.entity.EmployeeLeaveBalances;
@@ -9,6 +8,8 @@ import com.ciberspring.api.leaves_mgmt.service.AllEmployeesLeavesService;
 @Service
 public class AllEmployeesLeavesServiceImpl implements AllEmployeesLeavesService {
 	private final AllLeaveBalancesRepository allLeaveBalancesRepository;
+	
+	
 
 	public AllEmployeesLeavesServiceImpl(AllLeaveBalancesRepository allLeaveBalancesRepository) {
 		super();
